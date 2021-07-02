e70087d75863025fbbfe5cbc9a2119201fb27f56
brendan capuzzo <capuzzo_b@qwasar.io>
2021-07-02 14:40:36 +0000

    complete without comments

--- ex00/mastermind.c
+++ ex00/mastermind.c
@@ -3,28 +3,6 @@
 #include <time.h>
 #include <unistd.h>
 
-int my_strcmp(char* s1, char* s2) {
-    int ctr;
-    for (ctr = 0; s1[ctr] != 0 && s2[ctr] != 0; ctr++) { 
-        if ((int)s1[ctr] < (int)s2[ctr]) {
-            return -1;
-        }
-        else if ((int)s1[ctr] > (int)s2[ctr]) {
-            return 1;
-        }
-    }
-    if (s1[ctr] != 0 && s2[ctr] != 0) {
-        return 0;
-    }
-    else if (s1[ctr] != 0) {
-        return 1;
-    }
-    else if (s2[ctr] != 0) {
-        return -1;
-    }
-    return 0;
-}
-
 int wpp_calc(char* s1, char* s2) {
     int wpp_ctr = 0;
     for (int ctr = 0; s1[ctr] != 0 && s2[ctr] != 0; ctr++) { 
@@ -35,7 +13,6 @@ int wpp_calc(char* s1, char* s2) {
     return wpp_ctr;
 }
 
-// does not work as intended
 int mpp_calc(char* s1, char* s2) {
     int mpp_ctr = 0;
     for (int ctr = 0; s1[ctr] != 0 && s2[ctr] != 0; ctr++) { 
@@ -52,12 +29,11 @@ int mpp_calc(char* s1, char* s2) {
 int valid(char* s1) {
     int ctr = 0;
     for ( ; ctr < 4 && s1[ctr] != 0; ctr++) { 
-        // printf("Character value %c: %i\tctr: %d\n", s1[ctr], s1[ctr], ctr);
+        // confirms character is a digit between 0 and 7
         if ((int)s1[ctr] < 48 || (int)s1[ctr] > 55) {
             return 0;
         }
     }
-    // printf("%d\n", ctr);
     if (ctr < 4) {
         return 0;
     }
@@ -79,34 +55,33 @@ char* random_code_generator() {
     return secret;
 }
 
+// only numbers 0 through 7
 int main(int ac, char** av) {
     char* code = random_code_generator(); // randomly generated code
     int attempts = 10;
     for (int i = 1; i < ac; i += 2) {
         if (av[i][1] == 't') {
             attempts = atoi(av[i+1]);
+            if (attempts < 1) {
+                printf("Invalid number of attempts. Provide a positve integer greater than 0.\n");
+                return 0;
+            }
         }
         else if (av[i][1] == 'c') {
+            if (valid(av[i+1]) == 0) {
+                printf("Invalid Code. Provide a 4 digit code using numbers 0 through 7.\n");
+                return 0;
+            }
             code = av[i+1];
         }
     }
-
-    printf("Attempts: %d\nCode: %s\n", attempts, code);
-    // char input;
-    // Go through each attempt
+    printf("Will you find the secret code?\n");
+    // Iterate Through Attempts
     for (int att = 0; att < attempts; att++) {
-        // printf("att: %d /  attempts: %d\n", att, attempts);
         printf("---\nRound %d\n", att);
-        // char* guess = &input;
         char guess[100];
         while (1) {
-            // scanf("%s", &input); //cannot use scanf
-            
-            // char buffer[10];
-            // read(STDIN_FILENO, guess, 10);
             read(0, guess, 100);
-            // printf("%s\n", guess);
-
             // check if code is valid
             if (valid(guess) == 1) {
                 break;
@@ -115,7 +90,6 @@ int main(int ac, char** av) {
         }
         int wpp = wpp_calc(code, guess); // function to count well placed pieces
         int mpp = mpp_calc(code, guess); // function to count misplaced pieces
-        // if (my_strcmp(code, guess) == 0) { // guessed code correctly
         if (wpp == 4) {
             printf("Congratz! You did it!\n");
             break;
